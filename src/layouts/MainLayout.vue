c3369a1240548725de32fbe6676195651a6bc720
Tommy <118712788+jm-tommy@users.noreply.github.com>
2022-12-17 20:36:53 +0700

    init project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header bordered class="bg-white text-grey-10">
+      <q-toolbar class="constrain">
+        <q-btn
+          flat
+          round
+          dense
+          size="md"
+          icon="photo_camera"
+          to="/camera"
+          class="large-screen-only q-mr-sm"
+        />
+        <q-separator vertical spaced class="large-screen-only" />
+
+        <q-toolbar-title class="text-pacifico text-bold q-py-sm">
+          Quasagram
+        </q-toolbar-title>
+        <q-btn
+          flat
+          round
+          dense
+          size="md"
+          icon="home"
+          to="/"
+          class="large-screen-only"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white small-screen-only" bordered>
+      <q-tabs
+        active-color="primary"
+        indicator-color="transparent"
+        class="text-grey-10"
+      >
+        <q-route-tab to="/" icon="home" />
+        <q-route-tab to="/camera" icon="photo_camera" />
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  components: {},
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.q-toolbar__title {
+  font-size: 30px;
+  @media (max-width: $breakpoint-xs-max) {
+    text-align: center;
+  }
+}
+
+.q-footer {
+  .q-tab__icon {
+    font-size: 30px;
+  }
+}
+</style>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    Home Page
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageHome'
+}
+</script>
