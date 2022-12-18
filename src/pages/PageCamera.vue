0cec2d8d3e8454ea850ae12ac79879e3740e3d79
Tommy <118712788+jm-tommy@users.noreply.github.com>
2022-12-18 14:22:58 +0700

    feat(camera): design camera

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,11 +1,59 @@
 <template>
-  <q-page class="flex flex-center">
-    Camera Page
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="https://cdn.quasar.dev/img/mountains.jpg" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn color="grey-10" size="lg" round icon="photo_camera" />
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="post.caption"
+          class="col col-sm-6"
+          label="Caption"
+          dense
+        />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="post.location"
+          class="col col-sm-6"
+          label="Location"
+          dense
+        >
+          <template v-slot:append>
+            <q-btn round dense flat icon="location_on" />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn unelevated rounded color="primary" label="Post Image" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import { uid } from "quasar";
+
 export default {
-  name: 'PageCamera'
-}
+  name: "PageCamera",
+  data() {
+    return {
+      post: {
+        di: uid(),
+        caption: "",
+        location: "",
+        photo: null,
+        date: Date.now(),
+      },
+    };
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.camera-frame {
+  border: 2px solid $grey-10;
+  border-radius: 10px;
+}
+</style>
